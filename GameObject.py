b9fed6b380778bc2e478d1c2ff75b9ecea0bccfe
hejialehejiale <hejiale2012@hotmail.com>
2025-09-05 22:36:00 +0800

    我添加了game object.py
     我看了一下原来的文件，问了AI很多问题，我把entitysystem啃了下来，但我目前还未运用，几乎是懂了，但是json的那段读写文件我没写，旧项目有，但我感觉那一块我还不熟悉

---
+++ GameObject.py
@@ -0,0 +1,11 @@
+import pygame 
+class GameObject:#我需要一个空的箱子让我放东西或有东西的箱子拿东西
+	#由定义出来的classes生成的实例
+	def __init__(self, game, screen, name, pos, moveWithCamera):
+		self.game = game
+		self.screen = screen
+		self.name = name
+		self.pos = pos
+		self.moveWithCamera = moveWithCamera
+
+	
---
+++ Systems/EntitySystem.py
@@ -0,0 +1,35 @@
+import pygame
+from GameObject import GameObject
+class EntitySystem:#9.5 22：06 ：我开始做entity system
+	#由定义出来的classes生成的实例
+	def __init__(self, game ):
+		self.game = game
+		self.gameObjects = {}
+
+	def saveObject(self):
+		dict_saveObject = {}#设个列表用来存储object
+
+	def findObject(self, name):#寻找物体
+		if name in self.gameObjects:
+			return self.gameObjects[name]
+		
+		
+		
+		return None
+	def CreateObject(self, item):#创建物体
+		obj = None
+		if item['type'] == 'GameObject':
+			obj = self.CreateGameObject(item)
+
+		if obj is not None:
+			self.gameObjects[obj.name] = obj
+
+	def CreateGameObject(self, item):	#应用实例，呼应上文
+		    obj = GameObject(
+            game = self.game, 
+            screen = self.game.var主窗口, 
+            name = item['name'],
+            pos = item['pos'],
+            moveWithCamera = item['moveWithCamera']
+            #导入了我的gameobject
+        )
